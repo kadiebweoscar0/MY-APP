621f570943d99f566846d3a711e91ba32613481f
kadiebwe oscar <kadiebweoscar@gmail.com>
2023-11-12 01:55:02 +0100

    add a useSate on a component Cart and a button vider le panier

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,18 +1,28 @@
+import { useState } from 'react'
 import '../styles/Cart.css'
 
-export default function Cart() {
+
+function Cart() {
     const monsteraPrice = 8
-    const ivyPrice = 10
-    const flowerPrice = 15
-    return (
-        <div className="lmj-cart">
+    const [cart, updateCart] = useState(0)
+    const [isOpen, setIsOpen] = useState(false)
+ 
+    return isOpen ? (
+        <div className='lmj-cart'>
+            <button onClick={() => setIsOpen(false)}>Fermer</button>
             <h2>Panier</h2>
-            <ul>
-                <li>Monster : {monsteraPrice}$</li>
-                <li>Lierre : {ivyPrice}$</li>
-                <li>Fleurs: {flowerPrice}$</li>
-            </ul>
-            total : {monsteraPrice + ivyPrice + flowerPrice}$
+            <div>
+                Monstera : {monsteraPrice}€
+                <button onClick={() => updateCart(cart + 1)}>
+                    Ajouter
+                </button>
+                <button onClick={() => updateCart(0)}>Vider le panier</button>
+            </div>
+            <h3>Total : {monsteraPrice * cart}€</h3>
         </div>
+    ) : (
+        <button onClick={() => setIsOpen(true)}>Ouvrir le Panier</button>
     )
 }
+
+export default Cart
