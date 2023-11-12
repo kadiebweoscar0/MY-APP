76b351f5462e06949af10d9613b7b0fe10295156
kadiebwe oscar <kadiebweoscar@gmail.com>
2023-11-12 01:12:46 +0100

    add a component <Cart /> on component App

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
+import { Fragment } from "react";
 import Banner from "./Banner";
 import Footer from "./Footer";
 import ShoppingList from "./ShoppingList";
+import Cart from "./Cart";
 
 function App() {
   return (
-  <Banner/>,
-  <ShoppingList />,
-  <Footer />
+  <Fragment>
+      <Banner/>,
+      <Cart />,
+      <ShoppingList />,
+      <Footer />
+  </Fragment>
   )
 }
 
--- src/components/Hello.js
+++
@@ -1,7 +0,0 @@
-
-export default function Hello() {
-    return <div>
-        <p>Hello mokili nga yeyo naye déjà na bandi ekoyinda makasi {25*5}</p>
-    </div>
-}
-Hello()
